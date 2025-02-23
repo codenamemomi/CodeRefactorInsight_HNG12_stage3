e4c93c0704879810ab81adcfd88153b00a6bd134
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-23 13:29:18 +0100

    Update custom_setting to be required in integration JSON configuration

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def get_integration_json(request: Request):
             'author': 'codename',
             'settings': [
                 {"label": "interval", "type": "text", "required": True, "default": "0 * * * *"},
-                {"label": "custom_setting", "type": "text", "required": False, "default": ""}
+                {"label": "custom_setting", "type": "text", "required": True, "default": ""}
             ],
             'target_url': '',
             'tick_url': f'{base_url}/tick',
